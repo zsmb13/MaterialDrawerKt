c6b0dfb423b50f8453e4040643b82e7d6dd48897
zsmb13 <braunmarci@gmail.com>
2017-04-07 18:44:00 +0200

    Constructed a simple demo activity

--- app/src/main/java/co/zsmb/materialdrawerktexample/MainActivity.kt
+++ app/src/main/java/co/zsmb/materialdrawerktexample/MainActivity.kt
@@ -1,17 +1,57 @@
 package co.zsmb.materialdrawerktexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import co.zsmb.materialdrawerkt.DrawerBuilderKt
-import co.zsmb.materialdrawerkt.*
+import android.support.v7.app.AppCompatActivity
+import co.zsmb.materialdrawerkt.drawer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        drawer()
+        drawer {
+            accountHeader {
+                background = R.color.primary
+                profile("Mike Penz", "mikepenz@gmail.com") {
+                    icon = R.mipmap.ic_launcher_round
+                }
+                profile("Sally", "sally@sally.com") {
+                    icon = R.mipmap.ic_launcher
+                }
+                profile("Jimmy", "jimmy@jimmy.com") {
+                    icon = R.mipmap.ic_launcher
+                }
+            }
+            primaryItem("Primary 1", "Primary 1 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            primaryItem("Primary 2", "Primary 2 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            primaryItem("Primary 3", "Primary 3 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            divider()
+            secondaryItem("Secondary 1", "Secondary 1 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            secondaryItem("Secondary 2", "Secondary 2 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            secondaryItem("Secondary 3", "Secondary 3 description") {
+                icon = R.drawable.ic_heart
+                iconTintingEnabled = true
+            }
+            divider()
+        }
     }
 
 }
