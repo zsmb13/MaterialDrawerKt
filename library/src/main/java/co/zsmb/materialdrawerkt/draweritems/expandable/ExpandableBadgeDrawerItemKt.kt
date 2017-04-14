264ea6cc649fa8c5752fe71cbaa8267d26b9a8bd
zsmb13 <braunmarci@gmail.com>
2017-04-14 21:30:57 +0200

    ExpandableBadgeDrawerItem implemented, added Badgeable interface to enable advanced badging options

---
+++ library/src/main/java/co/zsmb/materialdrawerkt/draweritems/expandable/ExpandableBadgeDrawerItemKt.kt
@@ -0,0 +1,56 @@
+package co.zsmb.materialdrawerkt.draweritems.expandable
+
+import co.zsmb.materialdrawerkt.builders.BuilderBase
+import co.zsmb.materialdrawerkt.draweritems.badgeable.BadgeableKt
+import co.zsmb.materialdrawerkt.draweritems.base.BaseDescribeableDrawerItemKt
+import co.zsmb.materialdrawerkt.nonReadable
+import com.mikepenz.materialdrawer.holder.BadgeStyle
+import com.mikepenz.materialdrawer.holder.StringHolder
+import com.mikepenz.materialdrawer.model.ExpandableBadgeDrawerItem
+
+/**
+ * Adds a new ExpandableBadgeDrawerItem with the given [name] and [description].
+ * @return The created ExpandableBadgeDrawerItem instance
+ */
+fun BuilderBase.expandableBadgeItem(name: String = "",
+                                    description: String = "",
+                                    setup: ExpandableBadgeDrawerItemKt.() -> Unit = {}): ExpandableBadgeDrawerItem {
+    val item = ExpandableBadgeDrawerItemKt(name, description)
+    item.setup()
+    return item.build().apply { attachItem(this) }
+}
+
+class ExpandableBadgeDrawerItemKt(name: String, description: String) : BaseDescribeableDrawerItemKt(), BadgeableKt {
+
+    private val item = ExpandableBadgeDrawerItem()
+
+    init {
+        super.setItem(item)
+        item.withName(name)
+                .withDescription(description)
+    }
+
+    internal fun build() = item
+
+    // Documentation inherited
+    override var badgeRes: Int
+        get() = item.badge.textRes
+        set(value) {
+            item.withBadge(value)
+        }
+
+    // Documentation inherited
+    override var badgeHolder: StringHolder
+        get() = item.badge
+        set(value) {
+            item.withBadge(value)
+        }
+
+    // Documentation inherited
+    override var badgeStyle: BadgeStyle
+        get() = item.badgeStyle
+        set(value) {
+            item.withBadgeStyle(value)
+        }
+
+}
